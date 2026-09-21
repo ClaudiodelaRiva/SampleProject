e7ae4a792db90c2566ec2359628df07ae8c893bd
Claudio de la Riva <claudio@uniovi.es>
2026-09-21 19:49:15 +0200

    Refactor import statements for database configuration in main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from src.config import DATA_PATH, DB_PATH, SCHEMA_PATH
 from src.employees.employeeView import EmployeeView
-from src.util.database import Database
+import src.util.database
 
 def main():
     """Inicializa la base de datos y ejecuta la interfaz de empleados."""
-    db = Database(DB_PATH)
+    db = src.util.database.Database(DB_PATH)
     db.executeScript(SCHEMA_PATH)
     db.executeScript(DATA_PATH)
     EmployeeView().run()
